0630b768c1078479a53443aaef9399dcb388944a
yoales <alejandrosanchezmunoz@MBPdeAlejandro.home>
2021-01-24 13:34:19 +0100

    refactor: refactor Extractor & Uploader

--- src/GC_Extractor.py
+++ src/GC_Extractor.py
@@ -1,61 +1,66 @@
 from google.cloud import storage
 import os
+import sys
 import zipfile
 import configparser
 
-def download_blob(bucket_name, source_blob_name, destination_folder):
+def download_blob(bucket_name, source_blob_name, destination_path):
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
 
-    #Download blob from bucket into destination folder
+    # Download blob from bucket into destination folder
     blob = bucket.blob(source_blob_name)
-    blob.download_to_filename(destination_folder)
+    blob.download_to_filename(destination_path)
 
-    print('Blob {} downloaded to {}.'.format(source_blob_name, destination_folder))
+    print('Blob {} downloaded to {}.'.format(source_blob_name, destination_path))
 
-def download_all_blobs(bucket_name, destination_folder):
+def download_all_blobs(bucket_name, destination_path):
     storage_client = storage.Client()
     blobs = storage_client.list_blobs(bucket_name)
 
-    #Iterate over all blobs in bucket and download it into destination_folder
+    # Iterate over all blobs in bucket and download it into destination_path
     for blob in blobs:
-        download_blob(bucket_name, blob.name, destination_folder + blob.name)
+        download_blob(bucket_name, blob.name, destination_path + blob.name)
 
-def cleanFolder(folder, file_extension):
-    print('Delete all elements in {} with extension different than {}'.format(folder, file_extension))
-    files_in_directory = os.listdir(folder)
+def cleanpath(path, file_extension):
+    print('Delete all elements in {} with extension different than {}'.format(path, file_extension))
+    files_in_directory = os.listdir(path)
 
     filtered_files = [file for file in files_in_directory if not file.endswith(file_extension)]
     for file in filtered_files:
         print('Deleting file {}'.format(file))
-        path_to_file = os.path.join(folder, file)
+        path_to_file = os.path.join(path, file)
         os.remove(path_to_file)
 
-def unzipFilesInDir(folder, file_extension):
-    print('Unzip all files in {}'.format(folder))
-    #Loop through itens in zipFolder and unzip the .zip files. Then, delete zipped file
-    for item in os.listdir(folder):
+def unzipFilesInDir(path, file_extension):
+    print('Unzip all files in {}'.format(path))
+    # Loop through itens in zippath and unzip the .zip files. Then, delete zipped file
+    for item in os.listdir(path):
         if item.endswith(file_extension):
-            file_name = folder + "/" + item
+            file_name = path + "/" + item
             zip_ref = zipfile.ZipFile(file_name)
-            zip_ref.extractall(folder)
+            zip_ref.extractall(path)
             zip_ref.close()
             os.remove(file_name)
 
 if __name__ == '__main__':
     cp = configparser.ConfigParser()
-    cp.read('conf/conf.ini')
+    if(len(sys.argv) < 2):
+        print('Incorrect  number of parameters. This must be: \n'
+              '[conf_file.ini]')
+    else:
+        cp.read(sys.argv[1])
 
-    #Read the properties from the configuration file
-    google_app_credentials = cp.get('google', 'credentials')
-    bucket_name = cp.get('google', 'bucket_name')
-    data_folder = cp.get('extractor', 'data_folder')
-    file_extension = cp.get('extractor', 'file_extension')
+        # Read the properties from the configuration file
+        google_app_credentials = cp.get('google', 'credentials')
+        bucket_name = cp.get('google', 'bucket_name')
+        data_path = cp.get('extractor', 'raw_data_path')
+        file_extension = cp.get('extractor', 'raw_file_extension')
 
-    #Set GOOGLE_APPLICATION_CREDENTIALS environment variable to google credentials file
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = google_app_credentials
+        # Set GOOGLE_APPLICATION_CREDENTIALS environment variable to google credentials file
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = google_app_credentials
 
-    #Download all blobs stored in bucket
-    download_all_blobs(bucket_name, data_folder)
-    cleanFolder(data_folder, file_extension)
-    unzipFilesInDir(data_folder, file_extension)
+        # Download all blobs stored in bucket
+        download_all_blobs(bucket_name, data_path)
+        cleanpath(data_path, file_extension)
+        unzipFilesInDir(data_path, file_extension)
--- src/GC_Uploader.py
+++ src/GC_Uploader.py
@@ -1,12 +1,11 @@
 from google.cloud import bigquery
 from google.cloud.exceptions import NotFound
 import configparser
+import sys
 import os
 
-def getFilesInDir(folder):
-    os.listdir(folder)
-
 def create_dataset(client, dataset_id):
+    # Create dataset in BigQuery if not exists
     try:
         client.get_dataset(dataset_id)
         print("Dataset {} already exists".format(dataset_id))
@@ -14,6 +13,7 @@ def create_dataset(client, dataset_id):
         client.create_dataset(dataset_id)
 
 def create_table(client, table):
+    # Create table in BigQuery if not exists
     try:
         client.get_table(table)
         print("Table {} already exists".format(table))
@@ -36,36 +36,42 @@ def load_data(client, dataset_id, table_id, filename):
 
     print('Loaded {} rows into {}:{}.'.format(job.output_rows, dataset_id, table_id))
 
-def load_all_data(client, dataset_id, table_id, path):
-    # Get all files in path and call load_data function
+def load_all_data(client, dataset_id, table_id, path, file_extension):
+    # Get all files in path, filter by csv files and call load_data function
     for root, directories, files in os.walk(path, topdown=False):
         for name in files:
-            print('Loading file {} into table {}'.format(os.path.join(root, name), table_id))
-            load_data(client, dataset_id, table_id, os.path.join(root, name))
+            if(name.endswith(file_extension)):
+                print('Loading file {} into table {}'.format(os.path.join(root, name), table_id))
+                load_data(client, dataset_id, table_id, os.path.join(root, name))
 
 if __name__ == '__main__':
     cp = configparser.ConfigParser()
-    cp.read('conf/conf.ini')
+    if(len(sys.argv) < 2):
+        print('Incorrect  number of parameters. This must be: \n'
+              '[conf_file.ini]')
+    else:
+        cp.read(sys.argv[1])
 
-    # Read the properties from the configuration file
-    google_app_credentials = cp.get('google', 'credentials')
-    project_id = cp.get('google', 'project_id')
-    dataset_id = cp.get('google', 'dataset_id')
-    table_id = cp.get('google', 'table_id')
-    data_folder = cp.get('extractor', 'data_folder')
+        # Read the properties from the configuration file
+        google_app_credentials = cp.get('google', 'credentials')
+        project_id = cp.get('google', 'project_id')
+        dataset_id = cp.get('google', 'dataset_id')
+        table_id = cp.get('google', 'table_id')
+        data_path = cp.get('extractor', 'processed_data_path')
+        file_extension = cp.get('extractor', 'processed_file_extension')
 
-    # Set GOOGLE_APPLICATION_CREDENTIALS environment variable to google credentials file
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = google_app_credentials
+        # Set GOOGLE_APPLICATION_CREDENTIALS environment variable to google credentials file
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = google_app_credentials
 
-    # Create a client instance
-    client = bigquery.Client(project=project_id, location="US")
+        # Create a client instance
+        client = bigquery.Client(project=project_id)
 
-    # Create dataset if not exists
-    create_dataset(client, dataset_id)
+        # Create dataset if not exists
+        create_dataset(client, dataset_id)
 
-    # Create table if not exists
-    table = f"{project_id}.{dataset_id}.{table_id}"
-    create_table(client, table)
+        # Create table if not exists
+        table = f"{project_id}.{dataset_id}.{table_id}"
+        create_table(client, table)
 
-    # Load all data from path into BigQuery table
-    load_all_data(client, dataset_id, table_id, data_folder)
+        # Load all data from path into BigQuery table
+        load_all_data(client, dataset_id, table_id, data_path, file_extension)
